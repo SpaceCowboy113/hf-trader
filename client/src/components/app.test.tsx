92cd0a5a1818cd214fb556869f73d36101ccd95e
Jeffrey Bolin <jeffreybolin@jeffreys-mbp.lan>
2019-04-20 20:36:52 -0700

    fixed failing client test (app.test.tsx)

--- client/src/components/app.test.tsx
+++ client/src/components/app.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
-import ObservableMap from './state/observable-map';
-import { TradingModel, TradingRecord } from './state/trading-state';
+import App from './app';
+import ObservableMap from '../state/observable-map';
+import { TradingModel, TradingRecord } from '../state/trading-state';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
