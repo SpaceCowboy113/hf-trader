6372a97b93a8e1d13cb80c0250cd6b1dce16cdfe
Keenan M. Reimer <kreimer314@gmail.com>
2019-04-26 17:12:38 -0700

    More filter tuning

--- server/src/sliding_window.py
+++ server/src/sliding_window.py
@@ -38,7 +38,7 @@ def construct(
     maximum_size: int = 1,
     first_order_filter_time_constant: float = 1.0,
     second_order_filter_time_constant: float = 0.1,
-    filter_order_ratio: float = 0.1
+    filter_order_ratio: float = 0.33
     ) -> SlidingWindow:
     return SlidingWindow(
         samples=[],
